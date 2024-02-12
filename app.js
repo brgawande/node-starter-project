3fb49e7d84356ba0539d061c3064507ee3d4b554
Bhushan Gawande <brgawande@gmail.com>
2024-02-12 16:20:18 +0530

    added everthing

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 export default app;
